b5386673fb1a026186b1069c732ea1c539858d5a
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-09-10 07:59:29 -0600

    feat: Add VisualizarPropuestas component for displaying student proposals

---
+++ front/src/features/alumnos-propuestas/presentation/pages/VisualizarPropuestas.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VisualizarPropuestas: React.FC = () => {
+    return (
+        <h1>hola propuesta alumno</h1>
+    );
+};
+
+export default VisualizarPropuestas;
